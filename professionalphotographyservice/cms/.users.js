a8bc70530f5226d72624d4b0ec270f72f69512a1
Code Hustler <codehustlers@gmail.com>
2018-03-23 23:59:18 -0700

    first commit

---
+++ professionalphotographyservice/cms/.users.js
@@ -0,0 +1,11 @@
+({
+	users: [
+		{
+			username: 'halfnhav4',
+			tags: [],
+			salt: '506f57160a3150abe81b5e143abd7c5e',
+			hash: '1e6ec5fb6b2f787af8cbd7a11a9e5930c1cceb38526eea1fdf56513da58213d1',
+			user_created_timestamp: 1521864436500
+		}
+	]
+})
